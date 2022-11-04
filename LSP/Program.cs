8ca80746fbe54d9a6b206ab81b0e5f63b76ea0c6
Waseem Khalid <waseemsabra2@gmail.com>
2022-11-04 20:59:31 +0200

    Add project files.

---
+++ LSP/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+Student x = new Student();
+x.Add(1);
+
+
+
+public class Student
+{
+    public virtual  void ReadBook()
+    {
+        Console.WriteLine("Waseem .....");
+    }
+    public virtual void Add(int x)
+    {
+        Console.WriteLine(x + 1);
+    }
+}
+public class Read : Student
+{
+    public override void ReadBook()
+    {
+        Console.WriteLine("Child Class .....");
+    }
+
+    public override void Add(int x)
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
